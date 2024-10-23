710ee3a144330f16c30b014d987b96a777b26587
SANTINO AGUILERA <48313116@ort>
2024-10-23 11:36:46 -0300

    Worked on the Models and the Group Cards

--- Models/BD.cs
+++ Models/BD.cs
@@ -6,5 +6,4 @@ public static class BD
     
     /* Atributos */
     private static string _connectionString = @"Server=localhost;Database=DB_CommunyTalk;Trusted_Connection=True;";
-    public static List<Ejemplo> ListaPaises = new List<Ejemplo>();
 }
