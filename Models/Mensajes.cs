7719bb5b3279cec47d3d687739238a09b769c890
SANTINO AGUILERA <48313116@ort>
2024-10-23 11:03:33 -0300

    Added SearchCommunities.cshtml, SearchGroups.cshtml, Mensajes.cs and Usuarios.cs

---
+++ Models/Mensajes.cs
@@ -0,0 +1,14 @@
+public class Mensajes
+{
+    public int IdMensaje;
+    
+    public string Contenido;
+
+    public DateTime FechaHora;
+
+    public int IdUsuario;
+
+    public int IdChat;
+
+    public int Posicion;
+}
