028784561d47120207e23644224162ab855da13a
LEILA ALUMNE ENTENZA CHIDERSKI <48385438@ort>
2024-11-06 09:00:40 -0300

    botones terminados con animación bonita :)

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,18 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿gsap.fromTo(
+    ".Boton1",
+    {  x: -200 },
+    {  x: 20, duration: 1 }
+  );
+  gsap.fromTo(
+    ".Boton2",
+    {  x: -200 },
+    {  x: 20, duration: 1 }
+  );
+  gsap.timeline({
+    scrollTrigger: {
+      scrub: 1,
+      trigger: ".scroll-trigger-ready__worm-wrap",
+      start: "top 90%",
+      end: "bottom 30%",
+    },
+  });
